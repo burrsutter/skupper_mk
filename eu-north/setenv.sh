1838e7ddc2f8da2e3538ac075bdc116d0a812c9f
Burr Sutter <burrsutter@gmail.com>
2020-02-29 13:54:13 -0500

    adding env scripts

---
+++ eu-north/setenv.sh
@@ -0,0 +1,7 @@
+export PATH=/Users/burrsutter/11steps/bin:$PATH
+
+export KUBECONFIG="$(pwd)/.kube/config"
+echo $KUBECONFIG
+
+export KUBE_EDITOR="code -w"
+
---
+++ us-east/setenv.sh
@@ -0,0 +1,6 @@
+export PATH=/Users/burrsutter/11steps/bin:$PATH
+
+export KUBECONFIG="$(pwd)/.kube/config"
+echo $KUBECONFIG
+
+export KUBE_EDITOR="code -w"
